d5dce2784cd0808bd704b3cb20382455f0f38383
VampDude <stefan.neotokyo@gmail.com>
2017-02-02 16:19:04 +0100

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -2,6 +2,7 @@ let container = $('.lister-list tr');
 let movies = [];
 
 container.each(function () {
+    let rank = $(this).find('.titleColumn').text().trim().split(".")[0];
     let title = $(this).find('.titleColumn').find('a').text();
     let year = $(this).find('.titleColumn').find('span').text();
     let rating = $(this).find('.ratingColumn').find('strong').text();
@@ -9,6 +10,7 @@ container.each(function () {
     let link = $(this).find('.posterColumn').find('a').attr('href');
 
     if (title) movies.push({
+        rank,
         title,
         year,
         rating,
